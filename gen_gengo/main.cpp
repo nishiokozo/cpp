6a77535a8b1fcbeecf39b7cff00fd171c0430605
nishiokozo <nishiokozo@gmail.com>
2019-03-25 23:09:58 +0900

    常用漢字と画数対応

--- gen_gengo/main.cpp
+++ gen_gengo/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include <typeinfo>
+#include <boost/algorithm/string.hpp>
 using namespace std;
 
 void putKanji( int a )
@@ -11,27 +11,46 @@ void putKanji( int a )
 	char h = (a&0xff00)>>8;
 	cout.write(&h,1);
 	cout.write(&l,1);
-	cout << endl;	
 }
-int main()
+int main( int argc , char* argv[] )
 {
 	vector<string> tbl;
 
-	ifstream fi("JIS第一水準漢字_u8.txt");
+//	ifstream fi("JIS第一水準漢字.txt");
+	ifstream fi("JIS常用漢字.txt");
+//	ifstream fi("jistest.txt");
 
-	string buf;
-	while ( getline( fi, buf ) )
+	string line;
+	while ( getline( fi, line ) )
 	{
-		if( *(buf.end()-1) == 0x0d ) buf.pop_back();
-		tbl.push_back(buf);
+ 		if( *(line.end()-1) == 0x0d ) line.pop_back();
+
+		{
+			vector<string> tmp;
+			boost::split( tmp, line, boost::is_space() );
+			{
+				int n = 0 ;
+				if ( tmp.size() == 2 )
+				{
+					string s = tmp.begin()[1];
+					n = atoi(s.c_str());
+				}
+				if ( n <=10 )
+				{
+					tbl.push_back( tmp.begin()[0] );
+				}
+			}
+		}
+
 	}
 	
+//	for ( string& s2 : tbl ) cout << s2.c_str() << endl;
+
 	for ( string& s1 : tbl )
 	for ( string& s2 : tbl )
 	{
 		cout << s1.c_str() << s2.c_str() << endl;
 	}
-	
 
 }
 
